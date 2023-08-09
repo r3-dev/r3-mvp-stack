ae079fb8719bb9c0c963d715b2afc4bb616d6166
milanjrodd <milanrodd@gmail.com>
2023-08-09 20:29:46 +0400

    add automigrations and serve static

--- main.go
+++ main.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"os"
+	"strings"
 
-	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	// uncomment once you have at least one .go migration file in the "migrations" directory
+	// _ "yourpackage/migrations"
 )
 
 func main() {
 	app := pocketbase.New()
 
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// add new "GET /hello" route to the app router (echo)
-		e.Router.AddRoute(echo.Route{
-			Method: http.MethodGet,
-			Path:   "/hello",
-			Handler: func(c echo.Context) error {
-				return c.String(200, "Hello world!")
-			},
-			Middlewares: []echo.MiddlewareFunc{
-				apis.ActivityLogger(app),
-			},
-		})
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
 
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
 	})
 
