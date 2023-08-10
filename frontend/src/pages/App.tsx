6d315a74095e0d27d05b20fd36d3349e5e5a71f0
milanjrodd <milanrodd@gmail.com>
2023-08-10 15:23:41 +0400

    fix pocketbase naming

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Pocketbase + Vite + React</h1>
+      <h1>PocketBase + Vite + React</h1>
       <div className="card">
         <AuthShowcase />
         <p>
