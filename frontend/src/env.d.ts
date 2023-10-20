13c8d51c8de0721adf78849ec9a29f3724a203bb
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-10-20 15:28:34 +0800

    fix: pocketbase type

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="astro/client" />
 
-// type for astro.locals
 declare namespace App {
-    import type PocketBase from 'pocketbase';
-
     interface Locals {
-        pb: PocketBase,
+        pb: import('pocketbase').default
     }
 }
