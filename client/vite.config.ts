16257e36c69f568ff097fb11d217e99a2a6885c0
milanjrodd <milanrodd@gmail.com>
2024-03-03 01:33:09 +0300

    init the server config option for the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,4 +10,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8090",
+        changeOrigin: true,
+      },
+      "/_/": {
+        target: "http://127.0.0.1:8090",
+        changeOrigin: true,
+      },
+    },
+  },
 });
