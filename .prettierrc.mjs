1038ca52b94d3c064667febb62c7916550ff0dde
milanjrodd <milanrodd@gmail.com>
2024-03-03 11:35:41 +0300

    exclude crashnmax pretier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,9 +1,5 @@
-import prettierConfig from '@crashmax/prettier-config' assert { type: 'json' }
-
 export default {
-  ...prettierConfig,
   plugins: [
-    ...prettierConfig.plugins,
     'prettier-plugin-astro'
   ],
   overrides: [
