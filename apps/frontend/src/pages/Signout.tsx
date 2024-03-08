5527f8e454c824ea2e783aebb75a0fa50b6bf44d
milanjrodd <milanrodd@gmail.com>
2024-03-08 09:43:25 +0300

    rm depr

--- apps/frontend/src/pages/Signout.tsx
+++ apps/frontend/src/pages/Signout.tsx
@@ -1,9 +1,6 @@
 import { Api } from "@/App";
-import { useNavigate } from "@solidjs/router";
 
 export function SignOut() {
-  const navigate = useNavigate();
-
   Api.authStore.clear();
 
   return null;
